a845d90c9be9a659e1d743206a6630f492e092e4
Vihaan <87207836+VihaanSaini@users.noreply.github.com>
2022-02-14 17:04:52 -0800

    Create 2000iq.js

---
+++ 2000iq.js
@@ -0,0 +1,8 @@
+async function randomize(doing, langs, tools) {
+  let random1 = doing[Math.floor(doing.length * Math.random())];
+  let random2 = langs[Math.floor(langs.length * Math.random())];
+  let random3 = tools[Math.floor(tools.length * Math.random())];
+  return [random1, random2, random3];
+}
+
+exports.randomize = randomize
